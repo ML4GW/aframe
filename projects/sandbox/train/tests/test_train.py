dca06f69fe99c145d5ed545ceea8eb7e8d815213
William Benoit <william.benoit@deepclean.ldas.ligo-la.caltech.edu>
2022-12-09 17:44:53 -0600

    Getting all tests working except for signal contents

--- projects/sandbox/train/tests/test_train.py
+++ projects/sandbox/train/tests/test_train.py
@@ -135,36 +135,116 @@ def test_train(
     train_frac = 1 - valid_frac
     train_length = int(duration * train_frac * sample_rate - 1)
     train_background = background[:, :train_length]
-    assert train_dataset.X.numpy() == pytest.approx(train_background)
+    np.testing.assert_allclose(train_dataset.X.numpy(), train_background)
 
-    # Check that the shapes of the DataLoaders in validator are the right shape
+    # Validator
+    # Background DataLoader
+    # Check that the background loader has the right shape and data
     num_kernels = (duration * valid_frac) // (kernel_length - fduration) - 1
     num_kernels = int(num_kernels)
     kernel_size = kernel_length * sample_rate
+    background_loader = validator.background_loader.dataset[:][0].numpy()
 
+    # Check shape
     assert (
-        validator.background_loader.dataset[:][0].numpy().shape
+        background_loader.shape
         == np.array((num_kernels, num_ifos, kernel_size))
     ).all()
+    # Check that each kernel has mean 0
+    np.testing.assert_allclose(
+        np.mean(background_loader, axis=2),
+        np.zeros((num_kernels, num_ifos)),
+        atol=1e-01,
+    )
 
+    # Glitch DataLoader
     # This is set up to avoid any off by 1 errors that come from rounding
     total_valid_glitches = 2 * num_glitches - int(
-        (1 - valid_frac) * 2 * num_glitches
+        train_frac * 2 * num_glitches
     )
     num_coinc = int(
         glitch_prob**2 * total_valid_glitches / (1 + glitch_prob**2)
     )
     num_valid_glitches = total_valid_glitches - num_coinc
+    num_non_coinc = num_valid_glitches - num_coinc
+    h1_glitches = num_non_coinc // 2
+    glitch_loader = validator.glitch_loader.dataset[:][0].numpy()
+
+    # Check shape
     assert (
-        validator.glitch_loader.dataset[:][0].numpy().shape
+        glitch_loader.shape
         == np.array((num_valid_glitches, num_ifos, kernel_size))
     ).all()
 
+    # Check that the kernels are aranges (glitches) where we expect them to be
+    expected_h1_glitches = np.array(
+        [
+            np.arange(start, start + kernel_size)
+            for start in glitch_loader[:h1_glitches, 0, 0]
+        ]
+    )
+    expected_l1_glitches = -expected_h1_glitches
+
+    expected_h1_coinc = np.array(
+        [
+            np.arange(start, start + kernel_size)
+            for start in glitch_loader[num_non_coinc:, 0, 0]
+        ]
+    )
+    expected_l1_coinc = -expected_h1_coinc
+    expected_coinc_glitches = np.stack(
+        (expected_h1_coinc, expected_l1_coinc), axis=1
+    )
+
+    assert (glitch_loader[:h1_glitches, 0, :] == expected_h1_glitches).all()
     assert (
-        validator.signal_loader.dataset[:][0].numpy().shape
-        == np.array((int(valid_frac * num_waveforms), num_ifos, kernel_size))
+        glitch_loader[h1_glitches:num_non_coinc, 1, :] == expected_l1_glitches
+    ).all()
+    assert (
+        glitch_loader[num_non_coinc:, :, :] == expected_coinc_glitches
     ).all()
 
+    # Check that the kernels are background where we expect them to be
+    np.testing.assert_allclose(
+        np.mean(glitch_loader[:h1_glitches, 1, :], axis=1),
+        np.zeros(h1_glitches),
+        atol=1e-01,
+    )
+    np.testing.assert_allclose(
+        np.mean(glitch_loader[h1_glitches:num_non_coinc, 0, :], axis=1),
+        np.zeros(h1_glitches),
+        atol=1e-01,
+    )
+
+    # Signal DataLoader
+    # Check shape
+    signal_loader = validator.signal_loader.dataset[:][0].numpy()
+    num_valid_waveforms = int(valid_frac * num_waveforms)
+    assert (
+        signal_loader.shape
+        == np.array((num_valid_waveforms, num_ifos, kernel_size))
+    ).all()
+
+    # Check content
+    actual_signals = signal_loader - background_loader[: len(signal_loader)]
+    expected_h1_signals = np.array(
+        [
+            np.arange(start, start + kernel_size)
+            for start in actual_signals[:, 0, 0]
+        ]
+    )
+    expected_l1_signals = np.array(
+        [
+            np.arange(start, start + kernel_size)
+            for start in actual_signals[:, 1, 0]
+        ]
+    )
+    expected_signals = np.stack(
+        (expected_h1_signals, expected_l1_signals), axis=1
+    )
+    assert (actual_signals == expected_signals).all()
+
+    # Preprocessor
     # Check that the whitening filter is the correct shape
     assert (
         preprocessor.whitener.time_domain_filter.numpy().shape
