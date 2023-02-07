a301333751729161bd5b75e53b26c0a58534b046
Ethan Jacob Marx <ethan.marx@ligo.org>
2023-02-06 16:53:27 -0800

    add ability to specify which frame prior was defined in

--- libs/priors/bbhnet/priors/priors.py
+++ libs/priors/bbhnet/priors/priors.py
@@ -1,19 +1,18 @@
-from pathlib import Path
-from typing import TYPE_CHECKING, Optional, Sequence, Tuple
+from typing import TYPE_CHECKING, Optional
 
-import h5py
 import numpy as np
 from bilby.core.prior import (
     Constraint,
     Cosine,
     Gaussian,
-    Interped,
     LogNormal,
     PowerLaw,
     Sine,
     Uniform,
 )
 from bilby.gw.prior import BBHPriorDict, UniformSourceFrame
+
+from bbhnet.priors.utils import read_priors_from_file
 
 if TYPE_CHECKING:
     from astropy.cosmology import Cosmology
@@ -24,77 +23,34 @@ mpc = "Mpc"
 rad = "rad"
 
 
-def pdf_from_events(
-    param_values: Sequence[float],
-    grid_size: int = 100,
-    spacing: str = "lin",
-) -> Tuple[Sequence[float], Sequence[float]]:
-    """
-    Estimates the probability distribution of a parameter based on
-    a list of sampled values. Currently does this by just creating
-    a histogram of the values, but might consider doing a KDE in
-    the future
+class PriorDict(BBHPriorDict):
+    def __init__(self, *args, source_frame: bool = False, **kwargs):
+        super().__init__(*args, **kwargs)
+        # whether the priors are defined in source frame
+        self._source_frame = source_frame
 
-    Args:
-        param_values:
-            A list of parameter values drawn from the distribution
-            to be estimated
-        grid_size:
-            The number of points at which to estimate the pdf
-        spacing:
-            The spacing type of the grid, either linear or log
+    def sample(self, n: Optional[int] = None, source_frame: bool = False):
+        samples = super().sample(n)
+        # only rescale if requested frame is different from defined frame
+        rescale = self._source_frame != source_frame
+        if not rescale:
+            return samples
 
-    Returns:
-        grid:
-            The values at which the pdf was estimated
-        pdf:
-            The estimated pdf
-    """
-    param_min = np.min(param_values)
-    param_max = np.max(param_values)
-    if spacing == "lin":
-        bins = np.linspace(param_min, param_max, grid_size + 1)
-        grid = (bins[:-1] + bins[1:]) / 2
-    elif spacing == "log":
-        min_exp = np.log10(param_min)
-        max_exp = np.log10(param_max)
-        bins = np.logspace(min_exp, max_exp, grid_size + 1)
-        grid = np.sqrt(bins[:-1] * bins[1:])
-    else:
-        raise ValueError("Spacing must be either 'lin' or 'log'")
+        # convert to requested frame
+        factor = 1 + samples["redshift"]
+        factor = (1 / factor) if source_frame else factor
+        samples["mass_1"] *= factor
+        samples["mass_2"] *= factor
+        return samples
 
-    pdf, _ = np.histogram(param_values, bins, density=True)
-
-    return grid, pdf
+    # TODO: implement prob method that accounts for jacobian
+    # which will be used during importance sampling
+    def prob(self):
+        raise NotImplementedError
 
 
-def read_priors_from_file(event_file: Path, *parameters: str) -> BBHPriorDict:
-    """
-    Reads in a file containing sets of GW parameters and
-    returns a set of interpolated priors
-    The expected structure is based off the event file from
-    here: https://dcc.ligo.org/T2100512
-
-    Args:
-        event_file: An hdf5 file containing event parameters
-        parameters: Optional, a list of parameters to read from the file
-
-    Returns:
-        prior: A BBHPriorDict with priors based on the event file
-    """
-    prior = BBHPriorDict()
-    with h5py.File(event_file, "r") as f:
-        events = f["events"]
-        field_names = parameters or events.dtype.names
-        for name in field_names:
-            grid, pdf = pdf_from_events(events[name])
-            prior[name] = Interped(grid, pdf, np.min(grid), np.max(grid))
-
-    return prior
-
-
-def uniform_extrinsic() -> BBHPriorDict:
-    prior = BBHPriorDict()
+def uniform_extrinsic(source_frame: bool = False) -> PriorDict:
+    prior = {}
     prior["dec"] = Cosine()
     prior["ra"] = Uniform(0, 2 * np.pi)
     prior["theta_jn"] = 0
@@ -103,13 +59,13 @@ def uniform_extrinsic() -> BBHPriorDict:
     return prior
 
 
-def nonspin_bbh(cosmology: Optional["Cosmology"] = None) -> BBHPriorDict:
-    prior = uniform_extrinsic()
+def nonspin_bbh(cosmology: Optional["Cosmology"] = None) -> PriorDict:
+    prior = PriorDict(dictionary=uniform_extrinsic(), source_frame=False)
     prior["mass_1"] = Uniform(5, 100, unit=msun)
     prior["mass_2"] = Uniform(5, 100, unit=msun)
     prior["mass_ratio"] = Constraint(0, 1)
     prior["luminosity_distance"] = UniformSourceFrame(
-        0, 2, unit=mpc, name="redshift", cosmology=cosmology
+        0, 2, unit=mpc, name="luminosity_distance", cosmology=cosmology
     )
     prior["psi"] = 0
     prior["a_1"] = 0
@@ -126,7 +82,7 @@ def end_o3_ratesandpops(cosmology: "Cosmology") -> BBHPriorDict:
     """
     `population prior`
     """
-    prior = uniform_extrinsic()
+    prior = PriorDict(dictionary=uniform_extrinsic(), source_frame=True)
     prior["mass_1"] = PowerLaw(alpha=-2.35, minimum=2, maximum=100, unit=msun)
     prior["mass_2"] = PowerLaw(alpha=1, minimum=2, maximum=100, unit=msun)
     prior["mass_ratio"] = Constraint(0.02, 1)
@@ -145,7 +101,7 @@ def end_o3_ratesandpops(cosmology: "Cosmology") -> BBHPriorDict:
 
 
 def power_law_dip_break():
-    prior = uniform_extrinsic()
+    prior = uniform_extrinsic(source_frame=True)
     event_file = "./event_files/\
         O1O2O3all_mass_h_iid_mag_iid_tilt_powerlaw_redshift_maxP_events_bbh.h5"
     prior |= read_priors_from_file(event_file)
@@ -157,6 +113,8 @@ def gaussian_masses(
     m1: float, m2: float, sigma: float, cosmology: Optional["Cosmology"] = None
 ):
     """
+    Population prior
+
     Constructs a gaussian bilby prior for masses.
     Args:
         m1: mean of the Gaussian distribution for mass 1
@@ -165,7 +123,7 @@ def gaussian_masses(
 
     Returns a BBHpriorDict
     """
-    prior_dict = BBHPriorDict()
+    prior_dict = PriorDict(source_frame=True)
     prior_dict["mass_1"] = Gaussian(name="mass_1", mu=m1, sigma=sigma)
     prior_dict["mass_2"] = Gaussian(name="mass_2", mu=m2, sigma=sigma)
     prior_dict["redshift"] = UniformSourceFrame(
@@ -179,9 +137,14 @@ def gaussian_masses(
     return prior_dict
 
 
-def log_normal_masses(m1: float, m2: float, sigma: float = 2):
+def log_normal_masses(
+    m1: float,
+    m2: float,
+    sigma: float = 2,
+    cosmology: Optional["Cosmology"] = None,
+):
     """
-    `Population prior`
+    Population prior
 
     Constructs a log normal bilby prior for masses.
     Args:
@@ -191,11 +154,11 @@ def log_normal_masses(m1: float, m2: float, sigma: float = 2):
 
     Returns a BBHpriorDict
     """
-    prior_dict = BBHPriorDict()
+    prior_dict = PriorDict(source_frame=True)
     prior_dict["mass_1"] = LogNormal(name="mass_1", mu=m1, sigma=sigma)
     prior_dict["mass_2"] = LogNormal(name="mass_2", mu=m2, sigma=sigma)
-    prior_dict["luminosity_distance"] = UniformSourceFrame(
-        name="luminosity_distance", minimum=100, maximum=3000, unit="Mpc"
+    prior_dict["redshift"] = UniformSourceFrame(
+        name="redshift", minimum=0, maximum=2, cosmology=cosmology
     )
     prior_dict["dec"] = Cosine(name="dec")
     prior_dict["ra"] = Uniform(
