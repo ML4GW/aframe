ML4GW/aframe-legacy

restructure train project for omnibus batch augmentor

projects/sandbox/train/train/utils.py
@@ -6,31 +6,13 @@ from typing import List, Optional, Tuple, TypeVar
 import h5py
 import numpy as np
 import torch
-from train.data_structures import (
-    BBHNetWaveformInjection,
-    GlitchSampler,
-    SignalInverter,
-    SignalReverser,
-)
+from train.augmentor import BBHNetBatchAugmentor
+from train.data_structures import GlitchSampler, SnrRescaler, SnrSampler
 
+import ml4gw.gw as gw
 from ml4gw.distributions import Cosine, LogNormal, Uniform
 
 Tensor = TypeVar("Tensor", np.ndarray, torch.Tensor)
-
-
-class MultiInputSequential(torch.nn.Sequential):
-    """
-    Cheap wrapper around the torch Sequential object
-    to support multiple input
-    """
-
-    def forward(self, *inputs):
-        for module in self._modules.values():
-            if isinstance(inputs, tuple):
-                inputs = module(*inputs)
-            else:
-                inputs = module(inputs)
-        return inputs
 
 
 def split(X: Tensor, frac: float, axis: int) -> Tuple[Tensor, Tensor]:
@@ -65,10 +47,13 @@ def prepare_augmentation(
     swap_frac: float,
     mute_frac: float,
     sample_rate: float,
+    trigger_offset: float,
     highpass: float,
     mean_snr: float,
     std_snr: float,
     min_snr: float,
+    invert_prob: Optional[float] = 0.5,
+    reverse_prob: Optional[float] = 0.5,
     trigger_distance: float = 0,
     valid_frac: Optional[float] = None,
 ):
@@ -76,12 +61,15 @@ def prepare_augmentation(
     # glitches which will randomly insert them into
     # either or both interferometer channels
     glitch_dict = {}
-    valid_glitches_list = []
+    valid_glitches = []
 
     # calculate the time at which the validation set starts
     valid_start = (1 - valid_frac) * (
         train_val_stop - train_val_start
     ) + train_val_start
+
+    # split glitches into training and validation sets
+    # based on the time the background validation set starts
     with h5py.File(glitch_dataset, "r") as f:
         for ifo in ifos:
             glitches = f[ifo]["glitches"][:]
@@ -89,26 +77,25 @@ def prepare_augmentation(
 
             if valid_frac is not None:
                 train_glitches = glitches[times <= valid_start]
-                valid_glitches = glitches[times > valid_start]
+                ifo_glitches = glitches[times > valid_start]
                 logging.info(f"{len(train_glitches)} train glitches for {ifo}")
-                logging.info(f"{len(valid_glitches)} valid glitches for {ifo}")
+                logging.info(f"{len(ifo_glitches)} valid glitches for {ifo}")
                 glitch_dict[ifo] = train_glitches
-                valid_glitches_list.append(valid_glitches)
+                valid_glitches.append(ifo_glitches)
             else:
                 logging.info(f"{len(train_glitches)} train glitches for {ifo}")
                 glitch_dict[ifo] = glitches
-                valid_glitches_list = None
+                valid_glitches = None
 
-    glitch_inserter = GlitchSampler(
+    glitch_sampler = GlitchSampler(
         prob=glitch_prob,
         max_offset=int(trigger_distance * sample_rate),
         **glitch_dict,
     )
 
-    # initiate a waveform sampler from a pre-saved bank
-    # of GW waveform polarizations which will randomly
-    # project them to inteferometer responses and
-    # inject those resposnes into the input data
+    tensors, vertices = gw.get_ifo_geometry(*ifos)
+    # perform train/val split of waveforms,
+    # and compute fixed validation responses
     with h5py.File(waveform_dataset, "r") as f:
         signals = f["signals"][:]
         if valid_frac is not None:
@@ -116,48 +103,52 @@ def prepare_augmentation(
             valid_cross, valid_plus = valid_signals.transpose(1, 0, 2)
 
             slc = slice(-len(valid_signals), None)
-            valid_injector = BBHNetWaveformInjection(
-                ifos=ifos,
-                dec=f["dec"][slc],
-                psi=f["psi"][slc],
-                phi=f["ra"][slc],  # no geocent_time recorded, so just use ra
-                snr=None,
+            dec, psi, phi = f["dec"][slc], f["psi"][slc], f["phi"][slc]
+            valid_responses = gw.compute_observed_strain(
+                dec,
+                psi,
+                phi,
+                detector_tensors=tensors,
+                detector_vertices=vertices,
                 sample_rate=sample_rate,
-                highpass=highpass,
-                trigger_offset=0,
                 plus=valid_plus,
                 cross=valid_cross,
             )
-
         else:
-            valid_injector = None
+            valid_responses = None
 
     cross, plus = signals.transpose(1, 0, 2)
-    # instantiate source parameters as callable
-    # distributions which will produce samples
-    injector = BBHNetWaveformInjection(
-        ifos=ifos,
+    waveform_duration = cross.shape[-1] / sample_rate
+
+    # construct the augmentor that will be used at training time
+    # to sample waveforms, rescale snrs, insert glitches, perform
+    # background strain inversions and flips, etc.
+    rescaler = SnrRescaler(
+        len(ifos),
+        sample_rate,
+        waveform_duration,
+        highpass,
+    )
+
+    snr = SnrSampler(LogNormal(mean_snr, std_snr, min_snr))
+    augmentor = BBHNetBatchAugmentor(
+        ifos,
+        sample_rate,
+        mute_frac,
+        swap_frac,
+        glitch_downweight,
+        waveform_prob,
+        glitch_sampler,
         dec=Cosine(),
         psi=Uniform(0, pi),
         phi=Uniform(-pi, pi),
-        snr=LogNormal(mean_snr, std_snr, min_snr),
-        swap_frac=swap_frac,
-        mute_frac=mute_frac,
-        sample_rate=sample_rate,
-        highpass=highpass,
-        prob=waveform_prob,
-        glitch_prob=glitch_prob,
-        downweight=glitch_downweight,
-        trigger_offset=trigger_distance,
-        plus=plus,
+        trigger_offset=trigger_offset,
+        snr=snr,
+        rescaler=rescaler,
+        invert_prob=invert_prob,
+        reverse_prob=reverse_prob,
         cross=cross,
+        plus=plus,
     )
 
-    # stack glitch inserter and waveform sampler into
-    # a single random augmentation object which will
-    # be called at data-loading time (i.e. won't be
-    # used on validation data).
-    augmenter = MultiInputSequential(
-        glitch_inserter, SignalInverter(), SignalReverser(), injector
-    )
-    return augmenter, valid_glitches_list, valid_injector
+    return augmentor, valid_glitches, valid_responses

ML4GW/aframe

projects/sandbox/train/train/utils.py
@@ -6,31 +6,14 @@ from typing import List, Optional, Tuple, TypeVar
 import h5py
 import numpy as np
 import torch
-from train.data_structures import (
-    AframeWaveformInjection,
-    GlitchSampler,
-    SignalInverter,
-    SignalReverser,
-)
 
+from train.augmentor import AframeBatchAugmentor
+from train.data_structures import GlitchSampler, SnrRescaler, SnrSampler
+
+import ml4gw.gw as gw
 from ml4gw.distributions import Cosine, LogNormal, Uniform
 
 Tensor = TypeVar("Tensor", np.ndarray, torch.Tensor)
-
-
-class MultiInputSequential(torch.nn.Sequential):
-    """
-    Cheap wrapper around the torch Sequential object
-    to support multiple input
-    """
-
-    def forward(self, *inputs):
-        for module in self._modules.values():
-            if isinstance(inputs, tuple):
-                inputs = module(*inputs)
-            else:
-                inputs = module(inputs)
-        return inputs
 
 
 def split(X: Tensor, frac: float, axis: int) -> Tuple[Tensor, Tensor]:
@@ -65,10 +48,13 @@ def prepare_augmentation(
     swap_frac: float,
     mute_frac: float,
     sample_rate: float,
+    trigger_offset: float,
     highpass: float,
     mean_snr: float,
     std_snr: float,
     min_snr: float,
+    invert_prob: Optional[float] = 0.5,
+    reverse_prob: Optional[float] = 0.5,
     trigger_distance: float = 0,
     valid_frac: Optional[float] = None,
 ):
@@ -76,13 +62,12 @@ def prepare_augmentation(
     # glitches which will randomly insert them into
     # either or both interferometer channels
     glitch_dict = {}
-    valid_glitches_list = []
+    valid_glitches = []
 
     # calculate the time at which the validation set starts
     full_duration = train_stop - train_start
     train_duration = (1 - valid_frac) * full_duration
     valid_start = train_start + train_duration
-
     with h5py.File(glitch_dataset, "r") as f:
         for ifo in ifos:
             glitches = f[ifo]["glitches"][:]
@@ -90,26 +75,25 @@ def prepare_augmentation(
 
             if valid_frac is not None:
                 train_glitches = glitches[times <= valid_start]
-                valid_glitches = glitches[times > valid_start]
+                ifo_glitches = glitches[times > valid_start]
                 logging.info(f"{len(train_glitches)} train glitches for {ifo}")
-                logging.info(f"{len(valid_glitches)} valid glitches for {ifo}")
+                logging.info(f"{len(ifo_glitches)} valid glitches for {ifo}")
                 glitch_dict[ifo] = train_glitches
-                valid_glitches_list.append(valid_glitches)
+                valid_glitches.append(ifo_glitches)
             else:
                 logging.info(f"{len(train_glitches)} train glitches for {ifo}")
                 glitch_dict[ifo] = glitches
-                valid_glitches_list = None
+                valid_glitches = None
 
-    glitch_inserter = GlitchSampler(
+    glitch_sampler = GlitchSampler(
         prob=glitch_prob,
         max_offset=int(trigger_distance * sample_rate),
         **glitch_dict,
     )
 
-    # initiate a waveform sampler from a pre-saved bank
-    # of GW waveform polarizations which will randomly
-    # project them to inteferometer responses and
-    # inject those resposnes into the input data
+    tensors, vertices = gw.get_ifo_geometry(*ifos)
+    # perform train/val split of waveforms,
+    # and compute fixed validation responses
     with h5py.File(waveform_dataset, "r") as f:
         signals = f["signals"][:]
         if valid_frac is not None:
@@ -117,46 +101,52 @@ def prepare_augmentation(
             valid_cross, valid_plus = valid_signals.transpose(1, 0, 2)
 
             slc = slice(-len(valid_signals), None)
-            valid_injector = AframeWaveformInjection(
-                ifos=ifos,
-                dec=f["dec"][slc],
-                psi=f["psi"][slc],
-                phi=f["ra"][slc],  # no geocent_time recorded, so just use ra
-                snr=None,
+            dec, psi, phi = f["dec"][slc], f["psi"][slc], f["phi"][slc]
+            valid_responses = gw.compute_observed_strain(
+                dec,
+                psi,
+                phi,
+                detector_tensors=tensors,
+                detector_vertices=vertices,
                 sample_rate=sample_rate,
-                highpass=highpass,
-                trigger_offset=0,
                 plus=valid_plus,
                 cross=valid_cross,
             )
-
         else:
-            valid_injector = None
+            valid_responses = None
 
     cross, plus = signals.transpose(1, 0, 2)
-    # instantiate source parameters as callable
-    # distributions which will produce samples
-    injector = AframeWaveformInjection(
-        ifos=ifos,
+    waveform_duration = cross.shape[-1] / sample_rate
+
+    # construct the augmentor that will be used at training time
+    # to sample waveforms, rescale snrs, insert glitches, perform
+    # background strain inversions and flips, etc.
+    rescaler = SnrRescaler(
+        len(ifos),
+        sample_rate,
+        waveform_duration,
+        highpass,
+    )
+
+    snr = SnrSampler(LogNormal(mean_snr, std_snr, min_snr))
+    augmentor = AframeBatchAugmentor(
+        ifos,
+        sample_rate,
+        mute_frac,
+        swap_frac,
+        glitch_downweight,
+        waveform_prob,
+        glitch_sampler,
         dec=Cosine(),
         psi=Uniform(0, pi),
         phi=Uniform(-pi, pi),
-        snr=LogNormal(15, 15),
-        sample_rate=sample_rate,
-        highpass=highpass,
-        prob=waveform_prob,
-        glitch_prob=glitch_prob,
-        downweight=glitch_downweight,
-        trigger_offset=trigger_distance,
-        plus=plus,
+        trigger_offset=trigger_offset,
+        snr=snr,
+        rescaler=rescaler,
+        invert_prob=invert_prob,
+        reverse_prob=reverse_prob,
         cross=cross,
+        plus=plus,
     )
 
-    # stack glitch inserter and waveform sampler into
-    # a single random augmentation object which will
-    # be called at data-loading time (i.e. won't be
-    # used on validation data).
-    augmenter = MultiInputSequential(
-        glitch_inserter, SignalInverter(), SignalReverser(), injector
-    )
-    return augmenter, valid_glitches_list, valid_injector
+    return augmentor, valid_glitches, valid_responses