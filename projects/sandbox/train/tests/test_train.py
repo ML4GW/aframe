ba6c983a6135a5e9064481846aa99efb88c5e678
Alec Gunny <alec.gunny@gmail.com>
2023-05-31 17:37:06 -0700

    getting train tests passing

--- projects/sandbox/train/tests/test_train.py
+++ projects/sandbox/train/tests/test_train.py
@@ -5,6 +5,8 @@ import numpy as np
 import pytest
 from train.train import main as train
 
+from ml4gw.gw import compute_network_snr
+
 
 @pytest.fixture(params=[1024])
 def sample_rate(request):
@@ -120,9 +122,8 @@ def test_train(
     valid_frac,
     num_non_background,
     non_background_length,
-    waveforms,
 ):
-    num_waveforms = num_glitches = num_non_background
+    num_waveforms = num_non_background
     num_ifos = 2
     kernel_length = 2
     fduration = 1
@@ -170,114 +171,24 @@ def test_train(
     np.testing.assert_allclose(train_dataset.X.numpy(), train_background)
 
     # Validator
-    # Background DataLoader
-    # Check that the background loader has the right shape and data
-    num_kernels = (duration * valid_frac) // (kernel_length - fduration) - 1
-    num_kernels = int(num_kernels)
-    kernel_size = kernel_length * sample_rate
-    background_loader = validator.background_loader.dataset[:][0].numpy()
+    # check that we have the expected amount of validation data
+    assert validator.duration == (duration * valid_frac)
 
-    # Check shape
-    assert (
-        background_loader.shape
-        == np.array((num_kernels, num_ifos, kernel_size))
-    ).all()
-    # Check that each kernel has mean 0. atol is ~5 sigma
-    np.testing.assert_allclose(
-        np.mean(background_loader, axis=2),
-        np.zeros((num_kernels, num_ifos)),
-        atol=1.1e-01,
-    )
-
-    # Glitch DataLoader
-    # This is set up to avoid any off by 1 errors that come from rounding
-    total_valid_glitches = 2 * num_glitches - int(
-        train_frac * 2 * num_glitches
-    )
-    num_coinc = int(
-        glitch_prob**2 * total_valid_glitches / (1 + glitch_prob**2)
-    )
-    num_valid_glitches = total_valid_glitches - num_coinc
-    num_non_coinc = num_valid_glitches - num_coinc
-    h1_glitches = num_non_coinc // 2
-    glitch_loader = validator.glitch_loader.dataset[:][0].numpy()
-
-    # Check shape
-    assert (
-        glitch_loader.shape
-        == np.array((num_valid_glitches, num_ifos, kernel_size))
-    ).all()
-
-    # Check that the kernels are aranges (glitches) where we expect them to be
-    expected_h1_glitches = np.array(
-        [
-            np.arange(start, start + kernel_size)
-            for start in glitch_loader[:h1_glitches, 0, 0]
-        ]
-    )
-    expected_l1_glitches = -expected_h1_glitches
-
-    expected_h1_coinc = np.array(
-        [
-            np.arange(start, start + kernel_size)
-            for start in glitch_loader[num_non_coinc:, 0, 0]
-        ]
-    )
-    expected_l1_coinc = -expected_h1_coinc
-    expected_coinc_glitches = np.stack(
-        (expected_h1_coinc, expected_l1_coinc), axis=1
-    )
-
-    assert (glitch_loader[:h1_glitches, 0, :] == expected_h1_glitches).all()
-    assert (
-        glitch_loader[h1_glitches:num_non_coinc, 1, :] == expected_l1_glitches
-    ).all()
-    assert (
-        glitch_loader[num_non_coinc:, :, :] == expected_coinc_glitches
-    ).all()
-
-    # Check that the kernels are background where we expect them to be
-    np.testing.assert_allclose(
-        np.mean(glitch_loader[:h1_glitches, 1, :], axis=1),
-        np.zeros(h1_glitches),
-        atol=1.1e-01,
-    )
-    np.testing.assert_allclose(
-        np.mean(glitch_loader[h1_glitches:num_non_coinc, 0, :], axis=1),
-        np.zeros(h1_glitches),
-        atol=1.1e-01,
-    )
-
-    # Signal DataLoader
-    # Check shape
-    signal_loader = validator.signal_loader.dataset[:][0].numpy()
+    # check that we have the expected number of waveforms
     num_valid_waveforms = int(valid_frac * num_waveforms)
-    assert (
-        signal_loader.shape
-        == np.array((num_valid_waveforms, num_ifos, kernel_size))
-    ).all()
-
-    # Can't check signal content directly without knowing how the waveforms
-    # will be projected onto the ifos. But the polarizations are linear, so
-    # the ifo responses should also be linear, at least over a time frame of
-    # a couple seconds.
-    actual_signals = signal_loader - background_loader[: len(signal_loader)]
-    slopes = (actual_signals[:, :, -1] - actual_signals[:, :, 0]) / kernel_size
-    mean_slopes = np.mean(slopes, axis=0)
-    intercepts = actual_signals[:, :, 0]
-
-    expected_h1_signals = np.array(
-        [b + mean_slopes[0] * np.arange(kernel_size) for b in intercepts[:, 0]]
-    )
-    expected_l1_signals = np.array(
-        [b + mean_slopes[1] * np.arange(kernel_size) for b in intercepts[:, 1]]
-    )
-    expected_signals = np.stack(
-        (expected_h1_signals, expected_l1_signals), axis=1
+    waveform_size = int(non_background_length * sample_rate)
+    assert validator.waveforms.shape == (
+        num_valid_waveforms,
+        num_ifos,
+        waveform_size,
     )
 
-    # There's no precise reaoson for this tolerance
-    np.testing.assert_allclose(actual_signals, expected_signals, rtol=1e-6)
+    # check that our waveforms all have the correct snrs
+    psd = train_dataset.preprocessor.rescaler.background
+    snrs = compute_network_snr(
+        validator.waveforms, psd, sample_rate, highpass=32
+    )
+    assert (snrs > 6).all().item
 
     # Preprocessor
     # Check that the whitening filter is the correct shape
