bea2bc1c1af0a2baeb6f52eae20d9041af0917e5
William Benoit <benoi090@umn.edu>
2023-07-23 15:51:35 -0500

    Added documentation to priors

--- libs/priors/aframe/priors/utils.py
+++ libs/priors/aframe/priors/utils.py
@@ -7,10 +7,16 @@ from bilby.core.prior import Interped, PriorDict
 
 
 def chirp_mass(m1, m2):
+    """Calculate chirp mass from component masses"""
     return ((m1 * m2) ** 3 / (m1 + m2)) ** (1 / 5)
 
 
 def mass_condition_powerlaw(reference_params, mass_1):
+    """
+    Return a dictionary that can be interpreted by Bilby's
+    `ConditionalPowerLaw` to set the maximum value of `mass_2`
+    to be whatever was drawn for `mass_1`
+    """
     return dict(
         alpha=reference_params["alpha"],
         minimum=reference_params["minimum"],
@@ -19,6 +25,10 @@ def mass_condition_powerlaw(reference_params, mass_1):
 
 
 def mass_constraints(samples):
+    """
+    Return a dictionary with new keys `mass_ratio` and `chirp_mass`
+    so that Bilby `Constraint`s on these keys will be effected
+    """
     if "mass_1" not in samples or "mass_2" not in samples:
         raise KeyError("mass_1 and mass_1 must exist to have a mass_ratio")
     samples["mass_ratio"] = samples["mass_2"] / samples["mass_1"]
@@ -27,6 +37,7 @@ def mass_constraints(samples):
 
 
 def transpose(d: Dict[str, List]):
+    """Turn a dict of lists into a list of dicts"""
     return [dict(zip(d, col)) for col in zip(*d.values())]
 
 
@@ -36,7 +47,7 @@ def pdf_from_events(
     spacing: str = "lin",
 ) -> Tuple[Sequence[float], Sequence[float]]:
     """
-    Estimates the probability distribution of a parameter based on
+    Estimate the probability distribution of a parameter based on
     a list of sampled values. Currently does this by just creating
     a histogram of the values, but might consider doing a KDE in
     the future
@@ -82,11 +93,14 @@ def read_priors_from_file(event_file: Path, *parameters: str) -> PriorDict:
     here: https://dcc.ligo.org/T2100512
 
     Args:
-        event_file: An hdf5 file containing event parameters
-        parameters: Optional, a list of parameters to read from the file
+        event_file:
+            An hdf5 file containing event parameters
+        parameters:
+            Optional, a list of parameters to read from the file
 
     Returns:
-        prior: A PriorDict with priors based on the event file
+        prior:
+            A PriorDict with priors based on the event file
     """
     prior = PriorDict()
     with h5py.File(event_file, "r") as f:
