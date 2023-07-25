25025d8e3b2050488a9dd7499d48c34b94341052
wbenoit26 <90333821+wbenoit26@users.noreply.github.com>
2023-07-25 11:11:36 -0500

    Added documentation to logging (#377)

--- libs/logging/aframe/logging.py
+++ libs/logging/aframe/logging.py
@@ -7,6 +7,16 @@ from typing import Optional, Union
 def configure_logging(
     filename: Optional[Union[str, Path]] = None, verbose: bool = False
 ) -> None:
+    """
+    Set up logging format in a consistent way across projects
+
+    Args:
+        filename:
+            Name of file to which logging messages will be written
+        verbose:
+            If true, log at `DEBUG` verbosity, otherwise log at
+            `INFO` verbosity.
+    """
     log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(
         format=log_format,
