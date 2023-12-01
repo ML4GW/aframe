004f4653649a1d33697e2b8292df21159c58e986
Ethan Jacob Marx <ethan.marx@ligo.org>
2023-12-01 06:20:45 -0800

    get local export working in container

--- projects/export/export/cli.py
+++ projects/export/export/cli.py
@@ -1,21 +1,22 @@
-import jsonargparse
 import os
+
+import jsonargparse
 from export.main import export
+
 from utils.logging import configure_logging
+
 
 def build_parser():
     parser = jsonargparse.ArgumentParser()
-    parser.add_argument("--config", action=jsonargparse.ActionConfigFile)  
+    parser.add_argument("--config", action=jsonargparse.ActionConfigFile)
     parser.add_argument("--logfile", type=str, default=None)
     parser.add_function_arguments(export)
     return parser
 
+
 def main(args=None):
     parser = build_parser()
     args = parser.parse_args(args)
-
-    if args.outdir is not None:
-        os.makedirs(args.outdir, parents=True, exist_ok=True)
 
     if args.logfile is not None:
         logdir = os.path.dirname(args.logfile)
