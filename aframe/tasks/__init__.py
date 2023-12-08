45d8e8a75c4f49eaa8355bcc9976f513b83a8359
Ethan Jacob Marx <ethan.marx@ligo.org>
2023-12-07 19:02:54 -0800

    setup inference tasks

--- aframe/tasks/__init__.py
+++ aframe/tasks/__init__.py
@@ -1,3 +1,4 @@
 from .data import *
 from .export import ExportLocal
+from .infer import TritonServer
 from .train import TrainLocal
--- aframe/tasks/infer/infer.py
+++ aframe/tasks/infer/infer.py
@@ -1,19 +1,47 @@
 import os
+import subprocess
+from pathlib import Path
 
 import law
 import luigi
 import psutil
 from law.contrib import htcondor
+from luigi.util import inherits
 
+from aframe.base import AframeSandboxTask
 from aframe.tasks.data.timeslide_waveforms import MergeTimeslideWaveforms
 
+INFER_DIR = Path(__file__).parent.parent.parent / "projects" / "infer"
 
-class TritonServer(luigi.Task):
+
+def get_poetry_env(path):
+    output = subprocess.check_output(["poetry", "env", "info", "-p"], cwd=path)
+    return output.decode("utf-8").strip()
+
+
+ENV = get_poetry_env(INFER_DIR)
+
+
+class InferenceParams(law.Task):
+    ip_address = luigi.Parameter()
+    data_dir = luigi.Parameter()
+    inference_sampling_rate = luigi.FloatParameter()
+    batch_size = luigi.IntParameter()
+    psd_length = luigi.FloatParameter()
+    cluster_window_length = luigi.FloatParameter()
+    integration_window_length = luigi.FloatParameter()
+    fduration = luigi.FloatParameter()
+
+
+@inherits(InferenceParams)
+class Infer(law.SandboxTask):
     image = luigi.Parameter()
     model_repo_dir = luigi.Parameter()
     server_log = luigi.Parameter()
-    data_dir = luigi.Parameter()
     output_dir = luigi.Parameter()
+    triton_image = luigi.Parameter()
+
+    sandbox = "venv::{ENV}"
 
     @staticmethod
     def get_ip_address() -> str:
@@ -24,92 +52,64 @@ class TritonServer(luigi.Task):
         nets = psutil.net_if_addrs()
         return nets["enp1s0f0"][0].address
 
-    def output(self):
-        output = dict(
-            name=law.LocalFileTarget(
-                os.path.join(self.output_dir, "server_name.txt")
-            ),
-            ip=law.LocalFileTarget(
-                os.path.join(self.output_dir, "server_ip.txt")
-            ),
-        )
-        return output
-
     def run(self):
         from hermes.aeriel.serve import serve
 
-        # initialize server context, and enter it returning,
-        # returning a singularity container
-        # python instance pointing to the server.
-        # set wait to True to block until the server
-        # is live
+        # initialize a triton server contextmanager
         instance = serve(
-            self.image, self.model_repo_dir, self.server_log, wait=True
+            model_repo_dir=self.model_repo_dir,
+            image=self.triton_image,
+            log_file=self.server_log,
+            wait=True,
         )
-        instance = instance.__enter__
-        # write the server instance name and for
-        # downstream tasks to close the instance
-        with self.output()["name"]("w") as f:
-            f.write(instance.name)
+        # enter the context manager, and launch
+        # triton client jobs via conder
+        with instance:
+            yield Clients.req(self)
 
-        with self.output()["ip"]("w") as f:
-            f.write(self.get_ip_address())
+            # aggregate
 
 
-class TritonClients(htcondor.HTCondorWorkflow):
-    ip_address = luigi.Parameter(default="localhost")
-    data_dir = luigi.Parameter()
-    inference_sampling_rate = luigi.FloatParameter()
-    batch_size = luigi.IntParameter()
-    psd_length = luigi.FloatParameter()
-    cluster_window_length = luigi.FloatParameter()
-    integration_window_length = luigi.FloatParameter()
-    fduration = luigi.FloatParameter()
+@inherits(InferenceParams)
+class Clients(htcondor.HTCondorWorkflow, AframeSandboxTask):
+    clients_per_gpu = luigi.IntParameter()
 
     def create_branch_map(self):
-        segments = self.input()["segments"].load().splitlines()[1:]
-        branch_map, i = {}, 1
-        for row in segments:
-            row = row.split("\t")
-            start, duration = map(float, row[1::2])
-            step = duration if self.max_duration == -1 else self.max_duration
-            num_steps = (duration - 1) // step + 1
-
-            for j in range(int(num_steps)):
-                segstart = start + j * step
-                segdur = min(start + duration - segstart, step)
-                branch_map[i] = (segstart, segdur)
-                i += 1
-        return branch_map
+        # segments, shifts
+        return
 
     def workflow_requires(self):
-        return MergeTimeslideWaveforms()  # Fetch()
-
-    def requires(self):
-        return TritonServer()
+        return MergeTimeslideWaveforms()
 
     @property
     def tmp_dir(self):
         return os.path.join(self.data_dir, f"tmp-{self.branch}")
 
+    @property
+    def foreground_output(self):
+        return os.path.join(self.tmp_dir, "foreground.h5")
+
+    @property
+    def background_output(self):
+        return os.path.join(self.tmp_dir, "background.h5")
+
     def output(self):
-        return law.LocalFileTarget(
-            os.path.join(self.data_dir, "client_ips.txt")
-        )
+        return [
+            law.LocalFileTarget(self.foreground_output),
+            law.LocalFileTarget(self.background_output),
+        ]
 
     def run(self):
         from infer import infer
 
-        ip = self.input()["ip"].load()
         shifts, background_fname, injection_set_fname = self.branch_data
         infer(
-            ip=f"{ip}:8001",
+            ip=f"{self.ip}:8001",
             model_name="aframe",
             model_version=-1,
             shifts=shifts,
             background_fname=background_fname,
             injection_set_fname=injection_set_fname,
-            inference_sampling_rate=self.inference_sampling_rate,
             batch_size=self.batch_size,
             psd_length=self.psd_length,
             fduration=self.fduration,
@@ -117,16 +117,3 @@ class TritonClients(htcondor.HTCondorWorkflow):
             integration_window_length=self.integration_window_length,
             cluster_window_length=self.cluster_window_length,
         )
-
-
-class AggregateInfer(luigi.Task):
-    def requires(self):
-        return dict(clients=TritonClients(), server=TritonServer())
-
-    def output(self):
-        pass
-
-    def run(self):
-        # from spython import Client
-
-        pass
