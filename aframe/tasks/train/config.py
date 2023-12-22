12ae30c38880a2cadca1d97a9feb85494edb907d
Ethan Jacob Marx <ethan.marx@ligo.org>
2023-12-22 06:59:10 -0800

    add config for remote resource requests

--- aframe/tasks/train/config.py
+++ aframe/tasks/train/config.py
@@ -10,3 +10,11 @@ class wandb(luigi.Config):
     name = luigi.Parameter(default=os.getenv("WANDB_NAME", ""))
     group = luigi.Parameter(default=os.getenv("WANDB_GROUP", ""))
     tags = luigi.Parameter(default=os.getenv("WANDB_TAGS", ""))
+
+
+class train_remote(luigi.Config):
+    image = luigi.Parameter(default="ghcr.io/ml4gw/aframev2/train:main")
+    min_gpu_memory = luigi.IntParameter(default=15000)
+    request_gpus = luigi.IntParameter(default=4)
+    request_cpus = luigi.IntParameter(default=16)
+    request_cpu_memory = luigi.Parameter(default="32Gi")
