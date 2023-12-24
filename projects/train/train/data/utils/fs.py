d8f92d21155f633c6938afaa1b5a9fe67d1c032a
Ethan Marx <61295922+EthanMarx@users.noreply.github.com>
2023-12-24 12:09:15 -0600

    handle signal future properly :build: (#26)
    * incorporate s3 support to data jobs
    
    * update timeslide waveforms and export to take file like objects
    
    * get remote workflow running
    
    * have fetch inherit from query
    
    * move query task away from cli implementation
    
    * rename query --> segments
    
    * set wandb env variables in sandbox
    
    * add config for remote resource requests
    
    * fix EarlyStopping bug
    
    * multiprocess train data download
    
    * get mp downloading working
    
    * start signal download simulatnaeous with background files
    
    * add debugging statements
    
    * fix pre-commit
    
    * add pre-commit run to workflow
    
    * fix pre-commit
    
    * handle signal future properly :build:

--- projects/train/train/data/utils/fs.py
+++ projects/train/train/data/utils/fs.py
@@ -1,6 +1,7 @@
 import logging
 import os
-from concurrent.futures import ProcessPoolExecutor, wait
+from concurrent.futures import ProcessPoolExecutor
+
 from functools import partial
 from tempfile import gettempdir
 
@@ -121,6 +122,5 @@ def download_training_data(bucket: str, data_dir: str):
             download, f"{bucket}/{path}", f"{data_dir}/{path}"
         )
         executor.map(download, background_fnames, targets)
-
 
     future.result()
