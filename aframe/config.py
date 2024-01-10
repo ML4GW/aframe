2094c88e873ebb24d70860366322254216c7404d
Ethan Marx <61295922+EthanMarx@users.noreply.github.com>
2024-01-09 19:22:51 -0500

    Fixes to Tune :build: (#46)
    * get remote tune running with law / luigi
    
    * get hp tuning running
    
    * use node id for donwloading data instead of worker

--- aframe/config.py
+++ aframe/config.py
@@ -7,7 +7,7 @@ project_base = "/opt/aframe/projects"
 
 class ray_worker(luigi.Config):
     replicas = luigi.IntParameter(default=2)
-    gpus = luigi.IntParameter(default=2)
+    gpus_per_replica = luigi.IntParameter(default=2)
     cpus_per_gpu = luigi.IntParameter(default=8)
     gpus_per_worker = luigi.IntParameter(default=1)
     memory = luigi.Parameter(default="10G")
