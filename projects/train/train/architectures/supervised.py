b6ecea694324ab0dfc82d5a13fa2c5121d9a9b0b
Ethan Marx <61295922+EthanMarx@users.noreply.github.com>
2024-02-01 18:03:33 -0500

    Add Frequency domain architecture (#59)
    * update ml4gw to point to dev branch
    
    * add frequency domain dataset
    
    * point to 2d resnet commit
    
    * add supervised frequency domain network

--- projects/train/train/architectures/supervised.py
+++ projects/train/train/architectures/supervised.py
@@ -3,7 +3,8 @@ from typing import Literal, Optional
 from torch import Tensor
 from torchtyping import TensorType
 
-from ml4gw.nn.resnet_1d import NormLayer, ResNet1D
+from ml4gw.nn.resnet.resnet_1d import NormLayer, ResNet1D
+from ml4gw.nn.resnet.resnet_2d import ResNet2D
 from train.architectures import Architecture
 from train.architectures.networks import Xylophone
 
@@ -69,4 +70,26 @@ class SupervisedTimeDomainXylophone(Xylophone, SupervisedArchitecture):
         )
 
 
-# TODO: implement simple torchvision ResNet wrapper for frequency domain
+class SupervisedFrequencyDomainResNet(ResNet2D, SupervisedArchitecture):
+    def __init__(
+        self,
+        num_ifos: int,
+        layers: list[int],
+        kernel_size: int = 3,
+        zero_init_residual: bool = False,
+        groups: int = 1,
+        width_per_group: int = 64,
+        stride_type: Optional[list[Literal["stride", "dilation"]]] = None,
+        norm_layer: Optional[NormLayer] = None,
+    ) -> None:
+        super().__init__(
+            num_ifos,
+            layers=layers,
+            classes=1,
+            kernel_size=kernel_size,
+            zero_init_residual=zero_init_residual,
+            groups=groups,
+            width_per_group=width_per_group,
+            stride_type=stride_type,
+            norm_layer=norm_layer,
+        )
