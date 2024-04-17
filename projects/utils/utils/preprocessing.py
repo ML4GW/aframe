4b85b7e14cc9ffce31b8e18d654199b7523a0ae4
William Benoit <william.benoit@dgx1.ligo-wa.caltech.edu>
2024-04-16 18:55:56 -0700

    Set up envs and moved over files

--- projects/utils/utils/preprocessing.py
+++ projects/utils/utils/preprocessing.py
@@ -121,7 +121,7 @@ class BatchWhitener(torch.nn.Module):
             sample_rate,
             fftlength=fftlength,
             overlap=None,
-            average="mean",
+            average="median",
             fast=highpass is not None,
         )
         self.whitener = Whiten(fduration, sample_rate, highpass)
