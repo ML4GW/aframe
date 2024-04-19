183f75376030295720fd93c4c0f5996f6d0d492b
William Benoit <william.benoit@dgx1.ligo-wa.caltech.edu>
2024-04-19 13:09:59 -0700

    Added Q-transform args

--- projects/train/train/data/supervised/frequency_domain.py
+++ projects/train/train/data/supervised/frequency_domain.py
@@ -1,24 +1,31 @@
-from ml4gw.transforms.spectrogram import MultiResolutionSpectrogram
+from ml4gw.transforms.qtransform import SingleQTransform
 from projects.train.train.data.base import Tensor
 from train.data.supervised import SupervisedAframeDataset
 
 
 class FrequencyDomainSupervisedAframeDataset(SupervisedAframeDataset):
     def __init__(
-        self, *args, spectrogram: MultiResolutionSpectrogram, **kwargs
+        self,
+        *args,
+        qtransform: SingleQTransform,
+        num_f_bins: int,
+        num_t_bins: int,
+        **kwargs
     ):
         super().__init__(*args, **kwargs)
-        self.spectrogram = spectrogram
+        self.qtransform = qtransform
+        self.num_f_bins = num_f_bins
+        self.num_t_bins = num_t_bins
 
     def augment(self, X):
         X, y = super().augment(X)
-        X = self.spectrogram(X)
+        X = self.qtransform(X, self.num_f_bins, self.num_t_bins)
         return X, y
 
     def build_val_batches(
         self, background: Tensor, signals: Tensor
     ) -> tuple[Tensor, Tensor]:
         X_bg, X_fg = super().build_val_batches(background, signals)
-        X_bg = self.spectrogram(X_bg)
-        X_fg = self.spectrogram(X_fg)
+        X_bg = self.qtransform(X_bg, self.num_f_bins, self.num_t_bins)
+        X_fg = self.qtransform(X_fg, self.num_f_bins, self.num_t_bins)
         return X_bg, X_fg
