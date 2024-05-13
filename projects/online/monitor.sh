d80e5858794eeae5bd9b0355b52b64eb9aa07ff0
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-05-13 05:43:00 -0700

    run in apptainer image

--- projects/online/monitor.sh
+++ projects/online/monitor.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-config = ./config.yaml
+AFRAME_CONTAINER_ROOT=/home/william.benoit/aframe/images/
+config=./config.yaml
 log_dir=/home/william.benoit/online_monitoring
 mkdir -p $log_dir
 
 export CUDA_VISIBLE_DEVICES=2
 crash_count=0
-until poetry run python online/cli.py --config $config 2>> $log_dir/monitoring.log; do
+until apptainer run $AFRAME_CONTAINER_ROOT/online.sif poetry run python online/cli.py --config $config 2>> $log_dir/monitoring.log; do
     ((crash_count++))
     echo "Online deployment crashed on $(date) with error code $?, crash count = $crash_count" >> $log_dir/monitoring.log
     sleep 1
