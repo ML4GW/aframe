115049bef66b115d755806b836f67b1f0088cf65
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-06-10 12:50:22 -0700

    address wills last two comments

--- aframe/tasks/data/waveforms.py
+++ aframe/tasks/data/waveforms.py
@@ -162,7 +162,7 @@ class DeployValidationWaveforms(
         from ledger.injections import WaveformSet, waveform_class_factory
 
         cls = waveform_class_factory(
-            list(map(lambda x: x.lower(), self.ifos)),
+            self.ifos,
             WaveformSet,
             "IfoWaveformSet",
         )
