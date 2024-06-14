5250e3ab8f13579f15f7f28515e314ed65a34960
Ethan Marx <emarx@mit.edu>
2024-06-14 09:30:26 -0500

    add poetry command to run file for launching pipeline

--- scripts/aframe_init.py
+++ scripts/aframe_init.py
@@ -3,6 +3,7 @@
 import configparser
 import shutil
 from pathlib import Path
+from textwrap import dedent
 from typing import Optional
 
 import jsonargparse
@@ -50,8 +51,9 @@ def copy_configs(
     for config in configs:
         dest = path / config.name
         # update the config file to point to the paths
-        # of the config files in the init dir
+        # of other relevant config files in the init dir
         if config.suffix == ".cfg" and config.name != "base.cfg":
+            dest = path / f"{pipeline}.cfg"
             cfg = configparser.ConfigParser()
             cfg.read(config)
             cfg["core"]["inherit"] = str(path / "base.cfg")
@@ -74,23 +76,41 @@ def copy_configs(
             shutil.copy(config, dest)
 
 
-def create_env(path: Path, s3_bucket: Optional[Path] = None):
+def create_runfile(
+    path: Path, pipeline: str, s3_bucket: Optional[Path] = None
+):
     # if s3 bucket is provided
     # store training data and training info there
     base = path if s3_bucket is None else s3_bucket
-    content = f"""
-# Export environment variables
-export AFRAME_TRAIN_DATA_DIR={base}/data/train
-export AFRAME_TEST_DATA_DIR={path}/data/test
-export AFRAME_TRAIN_RUN_DIR={base}/training
-export AFRAME_CONDOR_DIR={path}/condor
-export AFRAME_RESULTS_DIR={path}/results
-export AFRAME_TMPDIR={path}/tmp/
-"""
 
-    env = path / "env.env"
+    config = path / f"{pipeline}.cfg"
+    # make the below one string
+    cmd = f"LAW_CONFIG_FILE={config} poetry run --directory $AFRAME_REPO "
+    cmd += f"law runaframe.pipelines.{pipeline}.{pipeline.capitalize()} "
+    cmd += "--workers 5 --gpus 0"
+    content = f"""
+    #!/bin/bash
+    # Export environment variables
+    export AFRAME_TRAIN_DATA_DIR={base}/data/train
+    export AFRAME_TEST_DATA_DIR={path}/data/test
+    export AFRAME_TRAIN_RUN_DIR={base}/training
+    export AFRAME_CONDOR_DIR={path}/condor
+    export AFRAME_RESULTS_DIR={path}/results
+    export AFRAME_TMPDIR={path}/tmp/
+
+    # launch pipeline; modify the gpus, workers etc. to suit your needs
+    # note that if you've made local code changes not in the containers
+    # you'll need to add the --dev flag!
+    {cmd}
+    """
+
+    content = dedent(content).strip("\n")
+    env = path / "run.sh"
     with open(env, "w") as f:
         f.write(content)
+
+    # make the file executable
+    env.chmod(0o755)
 
 
 def main():
@@ -137,7 +157,7 @@ def main():
         args.pipeline,
         args.s3_bucket,
     )
-    create_env(directory, args.s3_bucket)
+    create_runfile(directory, args.pipeline, args.s3_bucket)
 
 
 if __name__ == "__main__":
