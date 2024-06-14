f197fe51c5d9acf07e23f5436a799df11a55b999
Ethan Marx <emarx@mit.edu>
2024-06-14 10:14:06 -0500

    update readmes with aframe-init

--- scripts/aframe_init.py
+++ scripts/aframe_init.py
@@ -86,7 +86,7 @@ def create_runfile(
     config = path / f"{pipeline}.cfg"
     # make the below one string
     cmd = f"LAW_CONFIG_FILE={config} poetry run --directory $AFRAME_REPO "
-    cmd += f"law runaframe.pipelines.{pipeline}.{pipeline.capitalize()} "
+    cmd += f"law run aframe.pipelines.{pipeline}.{pipeline.capitalize()} "
     cmd += "--workers 5 --gpus 0"
     content = f"""
     #!/bin/bash
