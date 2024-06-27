f8b71fcccda81e18081c7f15915048cf8eeef999
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-06-27 06:54:04 -0700

    revert config to time domain

--- projects/train/train/data/supervised/time_domain.py
+++ projects/train/train/data/supervised/time_domain.py
@@ -1,3 +1,5 @@
+import torch
+
 from train.data.supervised.supervised import SupervisedAframeDataset
 
 
@@ -5,8 +7,14 @@ class TimeDomainSupervisedAframeDataset(SupervisedAframeDataset):
     def build_val_batches(self, background, signals):
         X_bg, X_inj, psds = super().build_val_batches(background, signals)
         X_bg = self.whitener(X_bg, psds)
-        X_inj = self.whitener(X_inj, psds)
-        return X_bg, X_inj
+        # whiten each view of injections
+        X_fg = []
+        for inj in X_inj:
+            inj = self.whitener(inj, psds)
+            X_fg.append(inj)
+
+        X_fg = torch.stack(X_fg)
+        return X_bg, X_fg
 
     def augment(self, X):
         X, y, psds = super().augment(X)
