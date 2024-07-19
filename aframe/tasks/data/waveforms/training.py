68e1d8536c827ad503cf29d496e05aa92deeb40b
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-07-19 12:14:57 -0700

    fix writing in training task when using s34

--- aframe/tasks/data/waveforms/training.py
+++ aframe/tasks/data/waveforms/training.py
@@ -54,57 +54,5 @@ class TrainingWaveforms(
             self.coalescence_time,
         )
         chunks = (min(64, num_signals), waveforms.get_waveforms().shape[-1])
-        waveforms.write(self.output().path, chunks=chunks)
-
-
-"""
-@inherits(DeployTrainingWaveforms)
-class TrainingWaveforms(AframeDataTask):
-    # Launch condorized generation of validation waveforms via
-    # rejection sampling, and merge results into a single file
-
-
-    output_dir = PathParameter(
-        description="Directory where merged training waveforms will be saved"
-    )
-    condor_directory = PathParameter(
-        default=os.path.join(
-            os.getenv("AFRAME_CONDOR_DIR", "/tmp/aframe/"),
-            "train_waveforms",
-        )
-    )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.output_file = self.output_dir / "train_waveforms.hdf5"
-
-    def output(self):
-        return s3_or_local(self.output_file)
-
-    def requires(self):
-        return DeployTrainingWaveforms.req(self)
-
-    @property
-    def targets(self):
-        return list(self.input().collection.targets.values())
-
-    @property
-    def waveform_files(self):
-        return list(map(Path, [targets.path for targets in self.targets]))
-
-    def run(self):
-        from ledger.injections import IntrinsicWaveformSet
-
-        chunks = (
-            min(64, self.num_signals),
-            int(self.waveform_duration * self.sample_rate),
-        )
-
-        with self.output().open("w") as f:
-            IntrinsicWaveformSet.aggregate(
-                self.waveform_files, f, clean=False, chunks=chunks
-            )
-
-        # clean up temporary directory
-        shutil.rmtree(self.tmp_dir)
-"""
+        with self.output.open("w") as f:
+            waveforms.write(f, chunks=chunks)
