13c7f0dd39a91383dc878bb422e747542de90901
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-07-25 13:51:08 -0700

    add defaults to paths config

--- aframe/config.py
+++ aframe/config.py
@@ -108,9 +108,7 @@ nautilus_urls = {
 
 
 class paths(luigi.Config):
-    train_datadir = PathParameter(
-        default=os.getenv("AFRAME_TRAIN_DATA_DIR", "")
-    )
+    train_datadir = PathParameter(default=os.getenv("AFRAME_TRAIN_DATA_DIR", ""))
     train_rundir = PathParameter(default=os.getenv("AFRAME_TRAIN_RUN_DIR", ""))
     results_dir = PathParameter(default=os.getenv("AFRAME_RESULTS_DIR", ""))
     test_datadir = PathParameter(default=os.getenv("AFRAME_TEST_DATA_DIR", ""))
