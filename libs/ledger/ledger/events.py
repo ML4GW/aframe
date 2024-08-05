a070a4361b8b0e7d187804e8a9b9a17d76422d02
Ethan Marx <61295922+EthanMarx@users.noreply.github.com>
2024-08-05 08:15:46 -0500

    Ledger event far updates (#228)
    * add threshold at far function
    
    * add threshold at far function
    
    * choose min_far instead of 0
    
    * use np.sort
    
    * use np.maximum

--- libs/ledger/ledger/events.py
+++ libs/ledger/ledger/events.py
@@ -59,12 +59,23 @@ class EventSet(Ledger):
             stats = self.detection_statistic[:, None]
             return (stats >= threshold).sum(0)
 
+    @property
+    def min_far(self):
+        """
+        Lowest FAR in Hz that can be resolved given background
+        livetime analyzed
+        """
+        return 1 / self.Tb * SECONDS_IN_YEAR
+
     def far(self, threshold: F) -> F:
         """
-        The false alarm rate for a given threshold
+        Far in Hz for a given detection statistic threshold, or
+        the minimum FAR that can be resolved
+        given the accumulated background livetime
         """
         nb = self.nb(threshold)
-        return SECONDS_IN_YEAR * nb / self.Tb
+        far = SECONDS_IN_YEAR * nb / self.Tb
+        return np.maximum(far, self.min_far)
 
     def significance(self, threshold: F, T: float) -> F:
         """see https://arxiv.org/pdf/1508.02357.pdf, eq. 17
@@ -80,6 +91,16 @@ class EventSet(Ledger):
 
         nb = self.nb(threshold)
         return 1 - np.exp(-T * (1 + nb) / self.Tb)
+
+    def threshold_at_far(self, far: float):
+        """
+        Return the detection statistic threshold
+        that corresponds to a given far in Hz
+        """
+        livetime = self.Tb
+        num_events = livetime * far
+        det_stats = np.sort(self.detection_statistic)
+        return det_stats[-int(num_events)]
 
     def apply_vetos(
         self,
