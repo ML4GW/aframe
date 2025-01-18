f602e9ffc9be3f50f2e0f40bb897ae5fca433805
Ethan Jacob Marx <ethan.marx@ligo.org>
2025-01-18 05:41:49 -0800

    hardcode scitoken auth use

--- projects/online/online/utils/gdb.py
+++ projects/online/online/utils/gdb.py
@@ -26,7 +26,7 @@ class GraceDb(_GraceDb):
     """
 
     def __init__(self, *args, write_dir: Path, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs, use_auth="scitoken")
         self.write_dir = write_dir
 
     def submit(self, event: Event):
