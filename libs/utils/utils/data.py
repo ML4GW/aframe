342fca459f721597875b66fe2bcad95952f5451b
Katrine Kompanets <katrine.kompanets@dgx1.ligo-wa.caltech.edu>
2025-02-12 10:24:32 -0800

    adjusted calc_shifts_required

--- libs/utils/utils/data.py
+++ libs/utils/utils/data.py
@@ -38,12 +38,11 @@ def calc_shifts_required(Tb: float, T: float, delta: float) -> int:
     combinations in front of the sum above.
     """
 
-    if Tb > 0:
-        discriminant = (delta / 2 - T) ** 2 - 2 * delta * Tb
-        N = (T - delta / 2 - discriminant**0.5) / delta
-        return math.ceil(N)
-    else:
+    if Tb == 0:
         return 0
+    discriminant = (delta / 2 - T) ** 2 - 2 * delta * Tb
+    N = (T - delta / 2 - discriminant**0.5) / delta
+    return math.ceil(N)
 
 
 def get_num_shifts_from_Tb(
