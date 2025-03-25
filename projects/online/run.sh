77a183b27e89a106cb1a2963266ed7485510015e
Ethan Jacob Marx <ethan.marx@ligo.org>
2025-03-25 12:49:42 -0700

    add weights env vars to command line

--- projects/online/run.sh
+++ projects/online/run.sh
@@ -24,7 +24,7 @@ config=/path/to/config.yaml
 
 export CUDA_VISIBLE_DEVICES=
 crash_count=0
-until apptainer run --nv --bind $XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR --env CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES --env AFRAME_ONLINE_OUTDIR=$AFRAME_ONLINE_OUTDIR --env ONLINE_DATADIR=$ONLINE_DATADIR --env AFRAME_WEIGHTS=$AFRAME_WEIGHTS --env AMPLFI_WEIGHTS=$AMPLFI_WEIGHTS $AFRAME_CONTAINER_ROOT/online.sif /opt/env/bin/online --config $config 2>> monitoring.log; do
+until apptainer run --nv --bind $XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR --env CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES --env AMPLFI_HL_WEIGHTS=$AMPLFI_HL_WEIGHTS --env AMPLFI_HLV_WEIGHTS=$AMPLFI_HLV_WEIGHTS--env AFRAME_ONLINE_OUTDIR=$AFRAME_ONLINE_OUTDIR --env ONLINE_DATADIR=$ONLINE_DATADIR --env AFRAME_WEIGHTS=$AFRAME_WEIGHTS --env AMPLFI_WEIGHTS=$AMPLFI_WEIGHTS $AFRAME_CONTAINER_ROOT/online.sif /opt/env/bin/online --config $config 2>> monitoring.log; do
     ((crash_count++))
     echo "Online deployment crashed on $(date) with error code $? from $(hostname),
     crash count = $crash_count" >> monitoring.log
