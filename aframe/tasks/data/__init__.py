cbb0709159d3cf08fc83b4e8ba6bcaa6c85b4f19
William Benoit <benoi090@umn.edu>
2025-03-30 13:00:17 -0700

    Update container definition files

--- aframe/tasks/data/__init__.py
+++ aframe/tasks/data/__init__.py
@@ -1,7 +1,3 @@
-from .fetch import Fetch
-from .segments import Query
-from .waveforms import TestingWaveforms, TrainingWaveforms, ValidationWaveforms
-
 DATAFIND_ENV_VARS = [
     "KRB5_KTNAME",
     "X509_USER_PROXY",
@@ -10,3 +6,6 @@ DATAFIND_ENV_VARS = [
     "LIGO_USERNAME",
     "DEFAULT_SEGMENT_SERVER",
 ]
+from .fetch import Fetch  # noqa: E402
+from .segments import Query  # noqa: E402
+from .waveforms import TestingWaveforms, TrainingWaveforms, ValidationWaveforms  # noqa: E402
