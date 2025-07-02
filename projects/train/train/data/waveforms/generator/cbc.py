86d82aa6790ac712758ed569a3203789f36d0b0a
William Benoit <benoi090@umn.edu>
2025-07-02 12:28:45 -0700

    Added waveform_sampler objects

---
+++ projects/train/train/data/waveforms/generator/cbc.py
@@ -0,0 +1,75 @@
+from typing import Callable
+
+import torch
+from ml4gw.waveforms.generator import TimeDomainCBCWaveformGenerator
+
+from ledger.injections import BilbyParameterSet
+
+from .generator import WaveformGenerator
+
+
+class CBCGenerator(WaveformGenerator):
+    def __init__(
+        self,
+        *args,
+        approximant: Callable,
+        f_min: float,
+        f_ref: float,
+        right_pad: float,
+        **kwargs,
+    ):
+        """
+        A lightweight wrapper around
+        `ml4gw.waveforms.generator.TimeDomainCBCWaveformGenerator`
+        to make it compatible with
+        `aframe.train.train.data.waveforms.generator.WaveformGenerator`.
+        Args:
+            *args:
+                Positional arguments passed to
+                `aframe.train.train.data.waveforms.generator.WaveformGenerator`
+            approximant:
+                A callable that takes parameter tensors
+                and returns the cross and plus polarizations.
+                For example, `ml4gw.waveforms.IMRPhenomD()`
+            f_min:
+                Lowest frequency at which waveform signal content
+                is generated
+            f_ref:
+                Reference frequency
+            right_pad:
+                Position in seconds where coalesence is placed
+                relative to the right edge of the window
+            **kwargs:
+                Keyword arguments passed to
+                `aframe.train.train.data.waveforms.generator.WaveformGenerator`
+        """
+        super().__init__(*args, **kwargs)
+        self.right_pad = right_pad
+        self.approximant = approximant
+        self.f_ref = f_ref
+        self.waveform_generator = TimeDomainCBCWaveformGenerator(
+            approximant,
+            self.sample_rate,
+            self.kernel_length,
+            f_min,
+            f_ref,
+            right_pad,
+        )
+
+    def convert(self, parameters):
+        # TODO: This assumes a detector-frame prior. Remove this
+        # when we switch to source-frame prior.
+        for key in ["mass_1", "mass_2", "chirp_mass", "total_mass"]:
+            if key in parameters:
+                parameters[key] *= 1 + parameters["redshift"]
+        parameter_set = BilbyParameterSet(**parameters)
+        generation_params = parameter_set.generation_params(
+            reference_frequency=self.f_ref
+        )
+        return generation_params
+
+    def forward(self, **parameters) -> torch.Tensor:
+        hc, hp = self.waveform_generator(**parameters)
+        waveforms = torch.stack([hc, hp], dim=1)
+        hc, hp = waveforms.transpose(1, 0)
+        return hc.float(), hp.float()
---
+++ projects/train/train/data/waveforms/sampler.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+
+import h5py
+import torch
+from utils import x_per_y
+
+Distribution = torch.distributions.Distribution
+
+
+class WaveformSampler(torch.nn.Module):
+    """
+    Base object defining methods that waveform producing classes
+    should implement. Should not be instantiated on its own.
+    Args:
+        fduration:
+            Desired length in seconds of the time domain
+            response of the whitening filter built from PSDs.
+            See `ml4gw.spectral.truncate_inverse_power_spectrum`
+        kernel_length:
+            Length in seconds of window passed to neural network.
+        sample_rate:
+            Sample rate in Hz of generated waveforms
+        val_waveform_file:
+            Path to the validation waveforms file.
+    """
+
+    def __init__(
+        self,
+        *args,
+        fduration: float,
+        kernel_length: float,
+        sample_rate: float,
+        val_waveform_file: Path,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.fduration = fduration
+        self.kernel_length = kernel_length
+        self.sample_rate = sample_rate
+        self.val_waveform_file = val_waveform_file
+
+        with h5py.File(val_waveform_file) as f:
+            key = list(f["waveforms"].keys())[0]
+            self.num_val_waveforms = len(f["waveforms"][key])
+
+    @property
+    def duration(self):
+        """
+        Length of kernel before whitening removes
+        fduration / 2 from each side
+        """
+        return self.fduration + self.kernel_length
+
+    def get_slice_bounds(self, total, world_size, rank) -> tuple[int, int]:
+        """
+        Determine waveform indices to load for this device
+        given our rank and world size
+        """
+        per_dev = x_per_y(abs(total), world_size)
+        start = rank * per_dev
+        stop = (rank + 1) * per_dev
+        return start, stop
+
+    # Assuming that we're going to be loading validation waveforms
+    # from disk for now, so this function can be defined here.
+    def get_val_waveforms(self, world_size, rank):
+        """
+        Returns validation waveforms for this device
+        """
+        start, stop = self.get_slice_bounds(
+            self.num_val_waveforms, world_size, rank
+        )
+        with h5py.File(self.val_waveform_file) as f:
+            waveforms = []
+            for key in f["waveforms"].keys():
+                waveforms.append(torch.Tensor(f["waveforms"][key][start:stop]))
+
+        return torch.stack(waveforms, dim=0)
+
+    def get_test_waveforms(self):
+        raise NotImplementedError
+
+    def sample(self):
+        """Defines how to sample waveforms for training"""
+        raise NotImplementedError
