c02fa1b5f9bc445636bd24ebc40d25f08a25c9f9
William Benoit <benoi090@umn.edu>
2026-09-21 10:48:10 -0400

    Correct resampling crop size

--- projects/online/online/dataloading/online.py
+++ projects/online/online/dataloading/online.py
@@ -95,10 +95,12 @@ def data_iterator(
         )
     factor = int(factor)
     b, a = build_resample_filter(factor, numtaps)
-    # Need to crop off at least half the filter size from
-    # both sides of the resampled data. Stick with powers of
-    # 2 to avoid issues coverting between time and samples.
-    crop_size = 2 ** np.ceil(np.log2((numtaps / 2) / factor))
+    # filtfilt runs the filter forwards and then backwards, so its
+    # effective kernel is `numtaps` samples wide on either side of
+    # each output. Crop that much from both sides of the resampled
+    # data. Stick with powers of 2 to avoid issues converting
+    # between time and samples.
+    crop_size = 2 ** np.ceil(np.log2(numtaps / factor))
     crop_length = crop_size / sample_rate
 
     frame_buffer = np.zeros((len(ifos), 0))
